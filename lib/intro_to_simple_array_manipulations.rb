d0cef99f03d6ac4299f786fb98adffeb07e21688
Blaine R Love <blainelove94@gmail.com>
2020-12-30 17:45:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,6 +4,6 @@ def using_concat(array, array2)
   array.concat(array2)
 end
 
-def using_insert(Array, element)
-  Array.insert(4, element)
+def using_insert(array, element)
+  array.insert(4, element)
 end
